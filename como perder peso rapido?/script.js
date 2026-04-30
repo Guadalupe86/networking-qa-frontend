ff279b93df0801df3040166b5650e06fd5082a47
Misael Antunez <gantunez895@gmail.com>
2026-04-29 20:51:32 -0600

    Delete como perder peso rapido? directory

--- como perder peso rapido?/script.js
+++
@@ -1,8 +0,0 @@
-// Smooth scroll when clicking on links
-document.querySelectorAll('nav ul li a').forEach(link => {
-    link.addEventListener('click', function (e) {
-        e.preventDefault();
-        const section = document.querySelector(this.getAttribute('href'));
-        section.scrollIntoView({ behavior: 'smooth' });
-    });
-});
